668e7c6c66794ef00a9d77c0716456e35f642c10
disco0 <disco0@users.noreply.github.com>
2019-07-26 04:11:03 -0700

    Update version/download hash (2.3.1.1), made description current

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName  = 'vibrancegui' # arbitrary name for the package, used in messages
 $url          = 'http://vibrancegui.com/vibrance/download' # download url
-$checksum     = 'cef3d364a76b6a93055b144bcd0a6844f73099e2'
+$checksum     = '52C9E0D6ECCC71FB71337124325D1BA0A3B2D502'
 $checksumType = 'sha1'
 
 Install-ChocolateyZipPackage $packageName `
